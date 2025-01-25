4a631458aca94897f50374b2969689c034f20a2b
TineshPandian <tineshpandian8@gmail.com>
2025-01-25 17:23:01 +0530

    cors

--- server.js
+++ server.js
@@ -9,11 +9,15 @@ dotenv.config();
 // Initialize Express app
 const app = express();
 connectDB();
-
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
 // Middleware
-app.use(cors()); // Enable CORS for all origins
+app.use(cors(corsOptions)); // Enable CORS for all origins
 app.use(express.json()); // Use express built-in JSON parser instead of body-parser
-
 // Recipe API routes
 app.use('/api/recipes', recipeRoutes);
 
